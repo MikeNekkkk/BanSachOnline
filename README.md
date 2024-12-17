6d8d9832b62f90175b22e52b6e9fd8adf12da8e8
Mai Huyền <maihuyenhi1>
2024-12-17 18:12:28 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+<<<<<<< HEAD
+# BanSachOnline
+Web bán sách online
+# Kết nối database
+Vào **inc/myconnect.php**
+sửa thông tin data base
+
+**File data base: ban_sach.sql**
+# Sửa code (bắt buộc)
+Vào file **product.php** tìm dòng 48
+
+`echo "<script>window.location.replace('http://host2.org/cart.php'); </script>";`
+
+sửa **host2.org** thành url của trang.
+=======
+# bansach
+>>>>>>> 4f2dcd6c08179fe3da95845307916ba5d2788456
---
+++ contact.php
@@ -0,0 +1,47 @@
+<?php 
+	include "head.php"
+	?>
+<?php
+$title ="Shop high";
+$name ="Điện thoai";
+?>
+<?php 
+	include "top.php"
+    ?>
+    <?php 
+	include "Header.php"
+	?>
+	<?php 
+	include "navigation.php"
+	?>
+	<!--//////////////////////////////////////////////////-->
+	<!--///////////////////Contact Page///////////////////-->
+	<!--//////////////////////////////////////////////////-->
+	<div id="page-content" class="single-page">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<ul class="breadcrumb">
+						<li><a href="index.php">Home</a></li>
+						<li><a href="contact.php">Contact</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="heading"><h1>Liên hệ với chúng tôi</h1></div>
+				</div>
+				<div class="col-md-6">
+					<p><span class="glyphicon glyphicon-home"></span>2Số 459, đường Tôn Đức Thắng, phường Hòa Khánh Nam, quận Liên Chiểu, TP. Đà Nẵng, Việt Nam.</p>
+					<p><span class="glyphicon glyphicon-earphone"></span> 012345678</p>
+					<p><span class="glyphicon glyphicon-envelope"></span> info.thanhtruong@gmail.com</p>
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.6500009099127!2d108.159124!3d16.0616474!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31421924682e8689:0x48eb0bdbeec05215!2zVHLGsOG7nW5nIMSQ4bqhaSBI4buNYyDEkGnhu4duIEzhu7Fj!5e0!3m2!1svi!2s!4v1602606781406!5m2!1svi!2s" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+						</div>
+			</div>
+		</div>
+	</div>
+	<?php 
+	include "footer.php"
+	?>
+</body>
+</html>
